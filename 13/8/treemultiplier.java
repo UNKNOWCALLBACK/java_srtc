7173df15ea0f0df445a77146959aa18e71fa9984
UNKNOWCALLBACK_TEAM <147795365+UNKNOWCALLBACK@users.noreply.github.com>
2024-08-13 05:36:20 +0000

    treemultiplier

---
+++ 13/8/treemultiplier.java
@@ -0,0 +1,9 @@
+public class treemultiplier {
+public static void (String[] args){
+      int multiplier = 3; 
+    
+        for (int i = 1; i <= 12; i++) {
+          System.out.println(multiplier + " * " + i + " = " + (multiplier * i));
+        }
+      }
+}
