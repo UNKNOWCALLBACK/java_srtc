91facaaad6667779bc9f8c8701af7105c6631c31
UNKNOWCALLBACK_TEAM <aliencity9966@gmail.com>
2024-08-13 11:56:48 +0700

    first commit

--- 13/8/multiplier.java
+++ 13/8/multiplier.java
@@ -2,7 +2,7 @@ public class multiplier {
     public static void main(String[] args) {
         int multiplier = 2; 
     
-        for (int i = 1; i <= 12; i++) {
+        for (int i = 1; i <= 13; i++) {
           System.out.println(multiplier + " * " + i + " = " + (multiplier * i));
         }
       }
