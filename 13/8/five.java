c379b8d5ed4d2aad9ed6dcd9d1665fcc1c405208
UNKNOWCALLBACK_TEAM <aliencity9966@gmail.com>
2024-08-13 12:08:00 +0700

    first commit

--- 13/8/five.java
+++ 13/8/five.java
@@ -2,7 +2,7 @@ public class five {
     public static void main(String[] args) {
     int x = 50;
   for (; x > 0; x -= 5) {
-        System.out.println(x);
+        System.out.print(x);
       }  
     }
   }
--- 13/8/ten.java
+++ 13/8/ten.java
@@ -2,7 +2,7 @@ public class ten {
     public static void main(String[] args) {
     int x = 0;
   for (; x < 110; x += 10) {
-        System.out.println(x);
+        System.out.print(x);
       }  
     }
   }
